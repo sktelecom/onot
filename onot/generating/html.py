5516849db19d9cd13c9afda3270c126a7a020937
cindy-lee <cindy.lee@daumkakao.com>
2023-01-10 19:58:39 +0900

    feat : 고지문 Output HTML file 개선

--- onot/generating/html.py
+++ onot/generating/html.py
@@ -28,37 +28,38 @@ class Generator():
         intro = 'A portion of this ' +  \
             doc['creationInfo']['organization'] + \
             ' product contains open source software, which is used and distributed in accordance with the specific license under which the open source software is distributed. A list of such open source software and the corresponding license terms is as follows:'
-        head = DOCTYPE + XMLNS + HEAD + STYLE_CSS 
-        body_title = BODY_TABLE_1 + title
+        head = DOCTYPE + XMLNS + HEAD + STYLE_CSS
+        body_title = BODY_TABLE_1 + '<h2>OSS Notice for' + title + '</h2>'
         body_intro = BODY_TABLE_2 + intro
         
         # component info field
         body_component = COMPONENT_TITLE
         for component in doc['packages']:
             name_version = component['name'] + ' ' + str(component['versionInfo'])
-            body_component += TABLE_ROW_1
-            body_component += '<div id="' + name_version.replace(' ', '_') + '"></div>'
+            body_component += '<tr>'
+            body_component += '<td scope="row" data-label="Name" id="' + name_version.replace(' ', '_') + '">'
             body_component += """
                 <a href="
-                """ + component['downloadLocation']+ """ 
+                """ + component['downloadLocation'] + """ 
                 " target="_blank">
                 """ + name_version + "</a>"
-            body_component += TABLE_ROW_2
-            body_component += self.convert_license_expression(component['licenseConcluded'])
-            body_component += TABLE_ROW_3
-            body_component += str(component['copyrightText'])
-            body_component += TABLE_CLOSE
+            body_component += '</td>'
+            body_component += '<td data-label="License">' + self.convert_license_expression(component['licenseConcluded']) + '</td>'
+            body_component += '<td data-label="Copyright">' + str(component['copyrightText']) + '</td>'
+            body_component += '</tr>'
+        body_component_close = TABLE_CLOSE
 
         # license text field
         body_license = LICENSE_TITLE
         for license in doc['licenses']:
-            body_license += '<h6><a id="' + license['licenseId'] + '">' + license['name'] + '</a></h6>'
+            body_license += '<div class="license">'
+            body_license += '<h4 id="' + license['licenseId'] + '">' + license['name']
             # component list
-            body_license += '<ul>'
             for package in license['packages']:
                 name_version = ' '.join(package)
-                body_license += '<li>' + '<a href = "#' + name_version.replace(' ', '_') + '">' + name_version + '</a></li>'
-            body_license += '</ul>'
+                body_license += '<a href = "#' + name_version.replace(' ', '_') + '">' + name_version + '</a>'
+            body_license += '</h4>'
+            body_license += '<div class="article">'
             # license text
             license_text = ''
             # if license['licenseTextHtml'] is None:
@@ -67,6 +68,8 @@ class Generator():
             #     license_text = license['licenseTextHtml']
             license_text = license['licenseText'].replace('\n', '<br>')
             body_license += '<p>' + license_text + '</p><br />'
+            body_license += '</div>'
+            body_license += '</div>'
         body_license += LICENSE_TABLE_CLOSE
         
         # writter
@@ -83,7 +86,7 @@ class Generator():
         # footer
         footer = FOOTER
 
-        return head + body_title + body_intro + body_component + body_license + body_offer + body_closing + footer
+        return head + body_title + body_intro + body_component + body_component_close + body_license + body_offer + body_closing + footer
 
     def generate_html_file(self, doc, html_code):
         now = datetime.now()
